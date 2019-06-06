62cee483a1a921caa9ac7e699617a0039e6df8b7
Darkensses <darkensses@gmail.com>
2019-06-05 22:56:14 -0500

    added getPlayers method preview

--- src/App.js
+++ src/App.js
@@ -1,61 +1,140 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as api from './api/api';
-import { Combobox, Button } from 'evergreen-ui'
+//import { Button } from 'evergreen-ui'
+import { Dropdown } from 'semantic-ui-react'
+import Table from 'antd/lib/table'; // for js
+import 'antd/lib/table/style/css';
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  render: text => <a href="#">{text}</a>,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+}];
+
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '4',
+  name: 'Disabled User',
+  age: 99,
+  address: 'Sidney No. 1 Lake Park',
+}];
 
 class App extends Component {
   
   state = {
     leagues: [],
-    teams: []
+    teams: [],
+    selectedRowKeys: [],
   }
 
-  getLeagues = async () => {
+  selectRow = (record) => {
+    const selectedRowKeys = [...this.state.selectedRowKeys];
+    if (selectedRowKeys.indexOf(record.key) >= 0) {
+      selectedRowKeys.splice(selectedRowKeys.indexOf(record.key), 1);
+    } else {
+      selectedRowKeys.push(record.key);
+    }
+    this.setState({ selectedRowKeys });
+  }
+  onSelectedRowKeysChange = (selectedRowKeys) => {
+    console.log(selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  }
+
+  getLeagues = async () => {    
     const leagues = await api.getLeagues();
     this.setState({leagues: leagues});
-    //console.log(this.state);
+    console.log(this.state);
   }
 
   getTeams = async (index) => {    
-    const teams = await api.getTeams(this.state.leagues[index].id);
+    //const teams = await api.getTeams(this.state.leagues[index].id);
+    const teams = await api.getTeams(index);
     this.setState({teams: teams});
-    //console.log(this.state);
+    console.log(this.state);
+  }
+
+  getPlayers = async (index) => {
+    await api.getPlayers(index);
   }
 
   getArrayValues = (arr) => {
     var auxArr = [];
     for(var key in Object.keys(arr)) {
-      auxArr.push(arr[key].value);
+      auxArr.push(
+        {
+          id: arr[key].id,
+          text: arr[key].value, 
+          value: arr[key].id,
+          image: { avatar: true, src: arr[key].img }
+        });
     }
+    console.log(auxArr)
     return auxArr;
   }
 
   render() {
     const auxLeagues = this.getArrayValues(this.state.leagues);
     const auxTeams = this.getArrayValues(this.state.teams);
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectedRowKeysChange,
+    };
     
     return (
       <div>
         <h1>Hi</h1>
-        <Button onClick={this.getLeagues}>Connect</Button>
-        <Combobox
-          items={auxLeagues}
-          onChange={selected => this.getTeams(auxLeagues.indexOf(selected))}
-          placeholder="Leagues"
-          autocompleteProps={{
-            // Used for the title in the autocomplete.
-            title: 'LEAGUE'
-          }}
+        <button onClick={this.getLeagues}>Connect</button>
+        <Dropdown
+          placeholder='Select League'
+          fluid
+          search
+          selection          
+          options={auxLeagues}
+          onChange={(e, selected) => this.getTeams(selected.value)}
         />
-        <Combobox
-          items={auxTeams}
-          onChange={selected => console.log(selected)}
-          placeholder="Teams"
-          autocompleteProps={{
-            // Used for the title in the autocomplete.
-            title: 'Team'
-          }}
+        <Dropdown
+          placeholder='Select Team'
+          fluid
+          search
+          selection
+          options={auxTeams}
+          onChange={(e, selected) => this.getPlayers(selected.value)}
         />
+
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={data}
+          onRow={(record) => ({
+            onClick: () => {
+              console.log(record);              
+            },
+          })}
+        />
+        
       </div>
     );
   }
--- src/api/api.js
+++ src/api/api.js
@@ -6,32 +6,46 @@ const proxy_url = 'https://cors-anywhere.herokuapp.com/';
 const base_url = 'https://sofifa.com';
 let html = '';
 
+// TODO: Add simple and full modes (simple returns {id, value} ; full returns {id, value, img})
+
 export async function getLeagues() {
     const res = await fetch(proxy_url + base_url + '/leagues');
     html = await res.text();
     var doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(html);
-    const nodes = xpath.select("//a[contains(@href,'/league/')]", doc);
+    //const nodes = xpath.select("//a[contains(@href,'/league/')]", doc);
+    const nodes = xpath.select("//table//tbody//tr", doc);
     
-    // Dictionary to save id league and its name
+    // Dictionary to save id league and its name    
     var dict = [];
 
     for(var i = 0; i < nodes.length; i++){
         doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(nodes[i].toString());
         var aux = xpath.select('//a/@href',doc);
         var id = aux[0].value.toString().replace('/league/','');
-        console.log((i+1) + ': ' + '(id:'+ id + ') ' + nodes[i].firstChild.data);
-        dict.push({id: id, value:nodes[i].firstChild.data});
+        aux = xpath.select("//a[contains(@href,'/league/')]",doc)
+        var name = aux[0].firstChild.data;
+        aux = xpath.select("//img[contains(@src,'https://cdn.sofifa.org/leagues/')]/@data-src",doc)
+        var imgSrc = aux[0].value;
+        //console.log(aux);
+        //console.log((i+1) + ': ' + '(id:'+ id + ') ' + nodes[i].firstChild.data);        
+        console.log((i+1) + ': ' + '(id:'+ id + ') ' + name);
+        //dict.push({id: id, value:nodes[i].firstChild.data, img:imgSrc});
+        dict.push({id: id, value:name, img:imgSrc});
     }
+    /*
     console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
     console.log("Node: " + nodes[0].toString());
+    console.log(nodes[0]);
+    */
     return dict;
 }
 
-export async function getTeams(id) {
-    const res = await fetch(proxy_url + base_url + '/teams?lg=' + id);
+export async function getTeams(idLeague) {
+    const res = await fetch(proxy_url + base_url + '/teams?lg=' + idLeague);
     html = await res.text();
     var doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(html);
-    const nodes = xpath.select("//a[contains(@href,'/team/')]", doc);
+    //const nodes = xpath.select("//a[contains(@href,'/team/')]", doc);
+    const nodes = xpath.select("//table//tbody//tr", doc);
     console.log(nodes)
     // Dictionary to save id Team and its name
     var dict = [];
@@ -40,8 +54,116 @@ export async function getTeams(id) {
         doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(nodes[i].toString());
         var aux = xpath.select('//a/@href',doc);
         var id = aux[0].value.toString().replace('/team/','');
-        dict.push({id: id, value:nodes[i].firstChild.data});
+        aux = xpath.select("//a[contains(@href,'/team/')]",doc)
+        var name = aux[0].firstChild.data;
+        aux = xpath.select("//img[contains(@src,'https://cdn.sofifa.org/teams/')]/@data-src",doc)
+        var imgSrc = aux[0].value;
+        //dict.push({id: id, value:nodes[i].firstChild.data});
+        dict.push({id: id, value:name, img:imgSrc});
     }
     return dict;
 }
 
+export async function getPlayers(idTeam) {
+    const res = await fetch(proxy_url + base_url + '/team/' + idTeam);
+    html = await res.text();
+    var doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(html);
+    const nodes = xpath.select("//table[preceding-sibling::h5[contains(text(),'Squad')]]/tbody/tr", doc);
+    //console.log(nodes);
+    var dict = [];
+
+    for(var i = 0; i < nodes.length; i++) {
+        doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(nodes[i].toString());
+        var aux = xpath.select("//a/@href",doc);        
+        var id = aux[1].value.toString().replace('/player/','').split('/')[0];
+        aux = xpath.select("//a[contains(@href,'/player/')]", doc);
+        var name = aux[0].firstChild.data;        
+        //console.log((i+1) + ': ' + '(id:'+ id + ') ' + name);   
+        var playerStats = await getPlayerStats(id);
+        dict.push({id: id, name: name, ...playerStats})
+    }
+    console.log(dict)
+}
+
+async function getPlayerStats(idPlayer) {
+    const res = await fetch(proxy_url + base_url + '/player/' + idPlayer);
+    html = await res.text();
+    var doc = new dom({errorHandler:{warning:(w) => {return}}}).parseFromString(html);
+    var dict = {};
+
+    // Attacking section
+    var nodesValue = xpath.select("//h5[contains(text(), 'Attacking')]/following-sibling::ul//li//span[1]//text()", doc);
+    var nodesSkill = xpath.select("//h5[contains(text(), 'Attacking')]/following-sibling::ul//li//span[2]//text()", doc);    
+    
+    for(let i=0; i < nodesValue.length; i++) {
+        //dict.push({[`${camelize(nodesSkill[i].data)}`]: nodesValue[i].data})
+        dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+
+    // Mentality section
+    nodesValue = xpath.select("//h5[contains(text(), 'Mentality')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Mentality')]/following-sibling::ul//li//span[2]//text()", doc);    
+
+    for(let i=0; i < nodesValue.length; i++) {
+        // Fix the compusre attribute
+        if(i === 5)
+            dict.composure = nodesValue[i].data;
+        else 
+            dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+
+    // Skill section
+    nodesValue = xpath.select("//h5[contains(text(), 'Skill')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Skill')]/following-sibling::ul//li//span[2]//text()", doc);    
+
+    for(let i=0; i < nodesValue.length; i++) {
+        dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+
+    // Defending section
+    nodesValue = xpath.select("//h5[contains(text(), 'Defending')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Defending')]/following-sibling::ul//li//span[2]//text()", doc);    
+
+    for(let i=0; i < nodesValue.length; i++) {
+        dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+
+    // Movement section
+    nodesValue = xpath.select("//h5[contains(text(), 'Movement')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Movement')]/following-sibling::ul//li//span[2]//text()", doc);    
+
+    for(let i=0; i < nodesValue.length; i++) {
+        dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+
+    // Goalkeeping section
+    nodesValue = xpath.select("//h5[contains(text(), 'Goalkeeping')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Goalkeeping')]/following-sibling::ul//li//span[2]//text()", doc);
+
+    // Fix the GK attributes
+    dict["gkDiving"] = nodesValue[0].data;
+    dict["gkHandling"] = nodesValue[1].data;
+    dict["gkKicking"] = nodesValue[2].data;
+    dict["gkPositioning"] = nodesValue[3].data;
+    dict["gkReflexes"] = nodesValue[4].data;
+
+    
+
+    // Power section
+    nodesValue = xpath.select("//h5[contains(text(), 'Power')]/following-sibling::ul//li//span[1]//text()", doc);
+    nodesSkill = xpath.select("//h5[contains(text(), 'Power')]/following-sibling::ul//li//span[2]//text()", doc);    
+
+    for(let i=0; i < nodesValue.length; i++) {
+        dict[`${camelize(nodesSkill[i].data)}`] = nodesValue[i].data;
+    }
+    
+    //console.log(dict);
+    return dict;
+}
+
+function camelize(text) {
+    return text.replace(/^([A-Z])|[\s-_]+(\w)/g, function(match, p1, p2, offset) {
+        if (p2) return p2.toUpperCase();
+        return p1.toLowerCase();        
+    });
+}
